26b60c25eacc2e64deea4d4c769fe7cc35044b0f
kareemeldahshoury <eldah001@umn.edu>
2025-11-08 17:04:34 -0600

    Basic UDP Sender

--- internals/udpSender/udpSender.go
+++ internals/udpSender/udpSender.go
@@ -3,10 +3,32 @@ package udpSender
 
 import (
 	"fmt"
+	"net"
 )
 
-func StartUDPSender() {
+func StartUDPSender(ip string, port string) {
 	fmt.Println("UDP Sender started")
+	address := ip + ":" + port
+	udpAddr, err := net.ResolveUDPAddr("udp", address) // Change the IP Address
+	if err != nil {
+		fmt.Println(err)
+		return;
+	}
+
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		fmt.Println(err)
+		return;
+	}
+	defer conn.Close()
+
+	message :=[]byte("TEST UDP CONN MESSAGE")
+	_, err = conn.Write(message)
+	if err != nil {
+		fmt.Println("Failed to write message to connection")
+		return;
+	}
+
 
 	// Block goroutine from exiting
 	for {}
